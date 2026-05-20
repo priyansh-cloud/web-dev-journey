373a7a320b9c3fdf139eb0be71d3b3b9fec44fc0
priyanshu-cloud <priyanshu.mahto7781@gmail.com>
2026-05-20 14:43:57 +0530

    updated

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -1,2 +1,17 @@
 // alert("welcome to priyanshu's page") ;
-console.dir(document.body.childNodes[1].);
+let h3 = document.querySelector("h3");
+console.dir(h3);
+h3.innerText = h3.innerText + " from apna college ";
+
+let divs = document.querySelectorAll(".box");
+divs[0].innerText = "Priyanshu";
+divs[1].innerText = "value 2";
+divs[2].innerText = "value 3";
+
+let idx = 1;
+for( div of divs){
+    div.innerText = `new value ${idx}`;
+    idx++;
+}
+console.log(divs);
+
